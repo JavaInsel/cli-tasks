2f7645c73e559b4235c04aec86b0b83330b21e00
JavaInsel <aveirohansen101@gmail.com>
2024-08-18 09:41:13 +0200

    Update functionality added

--- main.js
+++ main.js
@@ -3,40 +3,81 @@ const { argv } = require('node:process');
 //File System
 const fs = require('node:fs');
 
+//Global variable
+const filePath = 'c:\\Users\\Hansen\\Desktop\\tasks.json';
 
 console.log('Program start!');
-console.log('--------------------* * *--------------------');
+console.log('----------------------------------------* * *----------------------------------------');
 //process.argv returns array of arguments
 //slice(2) because the real arguments start from the second position
 userArgs = process.argv.slice(2);
 
-mode = userArgs[0];
-task = userArgs[1];
+const mode = userArgs[0];
 
-var tasks = [];
-
-var initial = {
-    "id":1,
-    "task":"Cleaning"
+if(!fs.existsSync(filePath)){
+    console.log('File doesn\'t exist!');
+    console.log('Creating a new file...');
+    writeToFile([]); //Initialize an empty array to the file
+    console.log('File has been created.')
 }
-tasks.push(initial);
+
+var tasks = readFile(filePath);
 
 if(mode=='add'){
+    const task = userArgs[1];
     var record = {
         "id":tasks.length+1,
-        "task":task
+        "description":task,
+        "status":"",
+        "createdAt":new Date(),
+        "updatedAt":""
     }
     tasks.push(record);
+    writeToFile(tasks);
+    console.log(`Task added sucessfully! (ID:${record.id})`)
+}else if(mode=='list'){
+    listAllTasks(tasks);
+}else if(mode=='update'){
+    const id = userArgs[1];
+    const task = userArgs[2];
+    updateTask(id,task);
+}
 
-    
+
+function writeToFile(content){
     try {
-        fs.writeFileSync('c:\\Users\\Hansen\\Desktop\\tasks.txt', JSON.stringify(tasks));
-        // file written successfully
+        fs.writeFileSync(filePath, JSON.stringify(content));
     } catch (err) {
         console.error(err);
     }
-
-    console.log(`Task added sucessfully! (ID:${record.id})`)
 }
 
-console.log(tasks)
+function readFile(path){
+    try {
+        const data = fs.readFileSync(path, 'utf8');
+        const jsonData = JSON.parse(data);
+        return jsonData;
+      } catch (err) {
+        console.error(err);
+      }
+}
+
+function listAllTasks(tasks){
+    console.log('All tasks:');
+    tasks.forEach((task)=>{
+        console.log(task);
+    })
+}
+
+function updateTask(id, task){
+    let taskToUpdate = tasks[id-1];
+    let newTask = {...taskToUpdate,description:task,updatedAt:new Date()};
+    const newTasks = tasks.map((task)=>{
+        if(task.id==id)
+        {
+            return newTask
+        }
+        return task;
+    })
+    writeToFile(newTasks);
+}
